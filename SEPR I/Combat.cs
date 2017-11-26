66585c2d7ffac8bcaf3dca4e830b4ccea7bf4770
Alasdair KIRBY <kirbyalasdair@gmail.com>
2017-11-26 17:01:56 +0000

    Entered Battlefield class, started combat loop implementation

--- SEPR I/Combat.cs
+++ SEPR I/Combat.cs
@@ -8,9 +8,34 @@ namespace SEPR_I
 {
     public class Combat
     {
-        public void StartLoop(Character[] inGame)
+        public void StartLoop(Character[][] inGame)
         {
-                        
+            Battlefield battlefield= new Battlefield();
+            Character[] Humans = inGame[0];
+            Character[] Monsters = inGame[1];
+            int Exp;
+
+            battlefield.setUp(inGame);
+
+            while(Humans.Length>0 && Monsters.Length > 0)
+            {
+                Character[] Available = Humans;
+                for ( int i =1;1>3;i++) //let player choose who to use first?
+                {
+                    //choose by option
+                    Character current;
+                    Boolean move = false;
+                    Boolean action = false;
+
+                    //pick move/action
+                    
+
+                }
+            }
+
+
+            
+                
         }
     }
 }
